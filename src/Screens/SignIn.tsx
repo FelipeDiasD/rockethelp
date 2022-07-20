49ec3ea3e6470ddfddde2ace279c06c2e68a5444
FelipeDiasD <prado.dias@hotmail.com>
2022-07-20 16:44:25 -0300

    adding firebase

--- src/Screens/SignIn.tsx
+++ src/Screens/SignIn.tsx
@@ -1,6 +1,6 @@
 import {useState} from 'react'
-
-
+import auth from '@react-native-firebase/auth'
+import { Alert } from 'react-native'
 import {VStack, Heading, Icon, useTheme} from 'native-base'
 import { Envelope, Key } from 'phosphor-react-native'
 import Logo from '../assets/logo_primary.svg'
@@ -13,23 +13,32 @@ export function SignIn (){
   
 const { colors } = useTheme();
 
-const [name, setName] = useState('')
-const [password, setPassword] = useState('')
+ const [email, setEmail] = useState('')
+ const [password, setPassword] = useState('')
+
+function handleSignIn(){
+  if(!email || !password){
+    return Alert.alert("Entrar", "Insira o seu e-mail e senha")
+  }
+}
+
+
+
 
   return(
-  <VStack flex={1} alignItems = 'center' bg ='gray.600' px = {8} pt ={24}>
+   <VStack flex={1} alignItems = 'center' bg ='gray.600' px = {8} pt ={24}>
 
     <Logo />
 
     <Heading color = 'gray.100' fontSize = 'xl' mt = {20} mb = {6}>
-      Acesse sua conta {name}
+      Acesse sua conta 
     </Heading>
 
     <Input 
     mb={4}
     placeholder = 'e-mail'
     InputLeftElement = {<Icon as ={<Envelope color = {colors.gray[300]}/>} ml = {4} />}
-    onChangeText = {setName}/>
+    onChangeText = {setEmail}/>
 
     <Input 
     placeholder = 'senha' 
